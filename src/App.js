4dc95ed1507ded72e342657000e98370e4641594
mhai247 <minhhai24072001@gmail.com>
2023-04-10 12:25:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{textAlign:"center"}}>
+     <div className="header">Hi I am Minh Hai</div><br /><br />
+     <p className="body-text"> <span style={{fontWeight:"bold"}}>Hedspi student</span> | Hanoi University of Science and Technology</p><br />
+     <a href="https://github.com/mhai247">GITHUB</a>
+    </div>
+  );
+}
+
+export default App;
